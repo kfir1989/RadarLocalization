1a01b278988285bab8329b152899f0fe20e14f56
Kfir <kfir.twizer@gmail.com>
2022-09-21 21:04:17 +0300

    metrics

---
+++ map_utils.py
@@ -0,0 +1,94 @@
+from nuscenes.map_expansion.map_api import NuScenesMap
+import cv2
+import numpy as np
+from scipy.spatial.distance import cdist
+import scipy.ndimage.morphology
+
+def draw_line(mat, x0, y0, x1, y1, fill=255, inplace=False):
+    if not (0 <= x0 < mat.shape[0] and 0 <= x1 < mat.shape[0] and
+            0 <= y0 < mat.shape[1] and 0 <= y1 < mat.shape[1]):
+        raise ValueError('Invalid coordinates.')
+    if not inplace:
+        mat = mat.copy()
+    if (x0, y0) == (x1, y1):
+        mat[x0, y0] = fill
+        return mat if not inplace else None
+    # Swap axes if Y slope is smaller than X slope
+    transpose = abs(x1 - x0) < abs(y1 - y0)
+    if transpose:
+        mat = mat.T
+        x0, y0, x1, y1 = y0, x0, y1, x1
+    # Swap line direction to go left-to-right if necessary
+    if x0 > x1:
+        x0, y0, x1, y1 = x1, y1, x0, y0
+    # Write line ends
+    mat[x0, y0] = fill
+    mat[x1, y1] = fill
+    # Compute intermediate coordinates using line equation
+    x = np.arange(x0 + 1, x1)
+    y = np.round(((y1 - y0) / (x1 - x0)) * (x - x0) + y0).astype(x.dtype)
+    # Write intermediate coordinates
+    mat[x, y] = fill
+    if not inplace:
+        return mat if not transpose else mat.T
+    
+def smoothEdges(edges):
+    black = np.where(edges == 255)
+    white = np.where(edges == 0)
+    binary_map = np.copy(edges)
+    binary_map[black] = 10
+    binary_map[white] = 0
+
+    kernel=np.array([[1, 1, 1],[1, 10, 1],[1, 1, 1]])
+    output = cv2.filter2D(binary_map, -1, kernel)
+    connectors = np.where((output == 110) | (output == 100))
+    connectors = np.array(connectors).T
+    dists = cdist(connectors, connectors, 'euclidean') + np.diag(np.full(connectors.shape[0],100))
+    outputa = np.copy(edges)
+    if len(dists) > 0:
+        closest = np.argmin(dists,axis=1)
+        for i in range(connectors.shape[0]):
+            if dists[i,closest[i]] < 3:
+                outputa = draw_line(outputa, connectors[i,0], connectors[i,1], connectors[closest[i],0], connectors[closest[i],1], inplace=False)
+       
+    return outputa
+
+def getRoadBorders(nuscMap, worldRef, patchSize=200, layer_names=['drivable_area'],blur_area=(3,3),threshold1=0, threshold2=0.1, smooth=True):
+        patch_angle = 0
+        patch_size = patchSize
+        patch_box = (worldRef[0], worldRef[1], patch_size, patch_size)
+        canvas_size = (patch_size, patch_size)
+        map_mask = nuscMap.get_map_mask(patch_box, patch_angle, layer_names, canvas_size)
+        mask = map_mask[0]
+        mask_blur = cv2.GaussianBlur(mask, blur_area, 0)
+        edges = cv2.Canny(image=mask_blur, threshold1=threshold1, threshold2=threshold2) # Canny Edge Detection
+        if smooth:
+            edges = smoothEdges(edges)
+        
+        return edges
+    
+def getLayer(nuscMap, worldRef, patchSize=200, layer_names=['drivable_area'],blur_area=(3,3),threshold1=0.5, threshold2=0.7):
+        patch_angle = 0
+        patch_size = patchSize
+        patch_box = (worldRef[0], worldRef[1], patch_size, patch_size)
+        canvas_size = (patch_size, patch_size)
+        map_mask = nuscMap.get_map_mask(patch_box, patch_angle, layer_names, canvas_size)
+        mask = map_mask[0]
+        
+        return mask
+    
+def getCombinedMap_old(nuscMap, worldRef, patchSize=200, smooth=True):
+    road_borders = getRoadBorders(nuscMap=nuscMap, worldRef=worldRef, patchSize=patchSize, layer_names=['drivable_area'],smooth=smooth)
+    walkway = getRoadBorders(nuscMap=nuscMap, worldRef=worldRef, patchSize=patchSize, layer_names=['walkway'],smooth=smooth)
+    edges = road_borders | walkway
+    
+    return edges
+
+
+
+def getCombinedMap(nuscMap, worldRef, patchSize=200, smooth=True):
+    image1 = getLayer(nuscMap, worldRef, patchSize=patchSize, layer_names=['drivable_area'])
+    image2 = getLayer(nuscMap, worldRef, patchSize=patchSize, layer_names=['walkway'])
+    edges1 = image1 - scipy.ndimage.morphology.binary_dilation(image1)
+    edges2 = image2 - scipy.ndimage.morphology.binary_dilation(image2)
+    return edges1 | edges2
---
+++ metrics.py
@@ -0,0 +1,89 @@
+import numpy as np
+from numpy.polynomial import polynomial as P
+
+def innerProductPolynoms(f1, f2, xmin, xmax):
+        f1mf2 = P.polysub(f1,f2)
+        f1mf2 = P.polymul(f1mf2,f1mf2)
+        val = P.polyint(f1mf2,lbnd=xmin)
+        #print("val", val)
+        ip = np.abs(val[0] + val[1]*(xmax-xmin)+val[2]*(xmax-xmin)**2+val[3]*(xmax-xmin)**3+val[4]*(xmax-xmin)**4+val[5]*(xmax-xmin)**5)
+        return np.sqrt(ip)/abs(xmax-xmin)
+    
+def associatePolynomAndPrior(res, priors):
+    ass_list = []
+    polynoms = res[-1]
+    
+    for iprior,prior in enumerate(priors):
+        for ipol,polynom in enumerate(polynoms):
+            f1 = (prior[0],prior[1],prior[2])
+            f2 = (polynom["f"].c[2],polynom["f"].c[1],polynom["f"].c[0])
+            xmin = max(prior[3],polynom["x_start"])
+            xmax = min(prior[4],polynom["x_end"])
+            dist = innerProductPolynoms(f1,f2,xmin,xmax)
+            if dist < 2 and xmax-xmin > 5:
+                ass_list.append((iprior,ipol))
+                
+    return ass_list
+
+def ComputeCoeffErrors(res, prior, ipolynom):
+    #print(res[-1][ipolynom]["f"],prior)
+    a0_err = np.zeros([len(res), 3])
+    a1_err = np.zeros([len(res), 3])
+    a2_err = np.zeros([len(res), 3])
+    for iframe,frame in enumerate(res):
+        a0_err[iframe,0] = abs(frame[ipolynom]["f"].c[2] - prior[0])
+        a1_err[iframe,1] = abs(frame[ipolynom]["f"].c[1] - prior[1])
+        a2_err[iframe,2] = abs(frame[ipolynom]["f"].c[0] - prior[2])
+        
+    a0_mean_err = np.mean(a0_err)
+    a1_mean_err = np.mean(a1_err)
+    a2_mean_err = np.mean(a2_err)
+    print(f"ComputeCoeffErrors: mean err for prior {prior} = ({a0_mean_err},{a1_mean_err},{a2_mean_err})")
+    
+    
+def ComputePolynomDistance(res, prior, ipolynom):
+    dist = np.zeros(len(res))
+    for iframe,frame in enumerate(res):
+        
+        f1 = (prior[0],prior[1],prior[2])
+        f2 = (frame[ipolynom]["f"].c[2],frame[ipolynom]["f"].c[1],frame[ipolynom]["f"].c[0])
+        xmin = max(prior[3],frame[ipolynom]["x_start"])
+        xmax = min(prior[4],frame[ipolynom]["x_end"])
+        dist[iframe] = innerProductPolynoms(f1,f2,xmin,xmax)
+        #print("f1",f1,"f2",f2,"xmin",xmin,"xmax",xmax,"dist[iframe]",dist[iframe])
+        
+    dist_mean_err = np.mean(dist)
+    dist_max_err = np.max(dist)
+    print(f"ComputePolynomDistance: dist_mean_err for prior {prior} = {dist_mean_err}")
+    print(f"ComputePolynomDistance: dist_max_err for prior {prior} = {dist_max_err}")
+    
+def ComputeOverlappingArea(res, prior, ipolynom):
+    overlap = np.zeros(len(res))
+    for iframe,frame in enumerate(res):
+        xmin = max(prior[3],frame[ipolynom]["x_start"])
+        xmax = min(prior[4],frame[ipolynom]["x_end"])
+        
+        xminmin = min(prior[3],frame[ipolynom]["x_start"])
+        xmaxmax = max(prior[4],frame[ipolynom]["x_end"])
+        overlap[iframe] = np.abs(xmax-xmin)/np.abs(xmaxmax-xminmin)
+        
+    overlap_mean = np.mean(overlap)
+    overlap_min = np.min(overlap)
+    print(f"ComputeOverlappingArea: overlap_mean for prior {prior} = {overlap_mean}")
+    print(f"ComputeOverlappingArea: overlap_min for prior {prior} = {overlap_min}")
+    
+def ComputeNonOverlappingArea(res, prior, ipolynom):
+    nonoverlap = np.zeros(len(res))
+    for iframe,frame in enumerate(res):
+        dmin = prior[3]-frame[ipolynom]["x_start"]
+        dmax = frame[ipolynom]["x_end"] - prior[4]
+        
+        dmin = dmin if dmin > 0 else 0
+        dmax = dmin if dmax > 0 else 0
+        
+        nonoverlap[iframe] = dmax + dmin
+        
+    nonoverlap_mean = np.mean(nonoverlap)
+    nonoverlap_max = np.max(nonoverlap)
+    print(f"ComputeNonOverlappingArea: onon_verlap_mean for prior {prior} = {nonoverlap_mean}")
+    print(f"ComputeNonOverlappingArea: non_overlap_max for prior {prior} = {nonoverlap_max}")
